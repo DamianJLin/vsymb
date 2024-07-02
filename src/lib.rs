28626ba5c615d5cacdffb60585eb7577db6a35f5
Damian <31408307+DamianJLin@users.noreply.github.com>
2024-07-02 21:51:25 +1000

    Cleaning again

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,15 @@ use std::{collections::HashMap, collections::HashSet, usize};
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn check_code(codestr: &str) -> bool {
-    let mut codewords: HashMap<&str, u32> = HashMap::new();
+    let mut frequency: HashMap<&str, usize> = HashMap::new();
     for grapheme in codestr.graphemes(true) {
-        codewords
+        frequency
             .entry(grapheme)
             .and_modify(|count| *count += 1)
             .or_insert(1);
     }
     let mut code_valid: bool = true;
-    for (_key, val) in codewords.iter() {
+    for (_key, val) in frequency.iter() {
         if *val != 2 {
             code_valid = false;
         }
@@ -56,40 +56,21 @@ enum Resolution {
 // A valid code word for a chord diagram is a Vec<&str>, where every unique &str in the vector
 // appearing exactly twice.
 pub fn jsymb(code: Vec<&str>) -> i32 {
-    let n = code.len() / 2;
+    let n = (code.len() / 2) as usize;
 
     let mut cuml: i32 = 0;
 
-    let connected_index = create_index_to_index(code.clone());
+    let connected_index = create_index_to_index(&code);
 
-    let options = [Resolution::Double, Resolution::Erase];
-    for state in std::iter::repeat(options.iter())
-        .take(n as usize)
-        .multi_cartesian_product()
-    {
-        let grapheme_id = order_unique_graphemes(code.clone());
-
-        // Construct a HashMap assigning each index (each position around the chord diagram) to its
-        // corresponding resolution (doubling or deletion), in a particular state.
-        fn create_index_to_resolution(
-            state: &Vec<&Resolution>,
-            grapheme_id: &Vec<&str>,
-            code: &Vec<&str>,
-        ) -> HashMap<usize, Resolution> {
-            let mut index_to_resolution = HashMap::new();
-            for (i, grapheme_to_index) in code.iter().enumerate() {
-                for (j, grapheme_to_compare) in grapheme_id.iter().enumerate() {
-                    if grapheme_to_index == grapheme_to_compare {
-                        index_to_resolution.insert(i, *state[j]);
-                    }
-                }
-            }
-
-            index_to_resolution
-        }
+    let resolutions = [Resolution::Double, Resolution::Erase];
+    let states = std::iter::repeat(resolutions.iter())
+        .take(n)
+        .multi_cartesian_product();
+    for state in states {
+        let grapheme_id = order_unique_graphemes(&code);
 
         let mut visited: HashSet<usize> = HashSet::new();
-        let resolve_index = create_index_to_resolution(&state, &grapheme_id, &code);
+        let resolve_index = create_index_to_resolution(&code, &state, &grapheme_id);
         let mut ccs = 0;
 
         for i in 0..(2 * n) {
@@ -128,13 +109,32 @@ pub fn jsymb(code: Vec<&str>) -> i32 {
     cuml
 }
 
-fn order_unique_graphemes(code: Vec<&str>) -> Vec<&str> {
+// Returns a HashMap assigning each index (each position around the chord diagram) to its
+// corresponding resolution (doubling or deletion), in a particular state.
+fn create_index_to_resolution(
+    code: &Vec<&str>,
+    state: &Vec<&Resolution>,
+    grapheme_id: &Vec<&str>,
+) -> HashMap<usize, Resolution> {
+    let mut index_to_resolution = HashMap::new();
+    for (i, grapheme_to_index) in code.iter().enumerate() {
+        for (j, grapheme_to_compare) in grapheme_id.iter().enumerate() {
+            if grapheme_to_index == grapheme_to_compare {
+                index_to_resolution.insert(i, *state[j]);
+            }
+        }
+    }
+    index_to_resolution
+}
+
+// Takes a reference to a code for a chord diagram and returns a new code, in which each unique
+// grapheme appears only once. This is used to fix an order on the graphemes.
+fn order_unique_graphemes<'a>(code: &Vec<&'a str>) -> Vec<&'a str> {
     let mut index_to_grapheme = HashMap::new();
     for (i, g) in code.iter().enumerate() {
         index_to_grapheme.insert(i, *g);
     }
 
-    // Remove duplicates from code to order the graphemes.
     let mut code_nodup = code.clone();
     let mut uniques = HashSet::new();
     code_nodup.retain(|e| uniques.insert(*e));
@@ -142,7 +142,9 @@ fn order_unique_graphemes(code: Vec<&str>) -> Vec<&str> {
     code_nodup
 }
 
-pub fn create_index_to_index(code: Vec<&str>) -> HashMap<usize, usize> {
+// Takes a reference to a valid chord diagram code and returns a HashMap<usize, usize> that takes
+// each index to the other index in the code with the same grapheme.
+fn create_index_to_index(code: &Vec<&str>) -> HashMap<usize, usize> {
     let mut index_to_index = HashMap::new();
     let mut temp = HashMap::new();
     for (i, g) in code.iter().enumerate() {
